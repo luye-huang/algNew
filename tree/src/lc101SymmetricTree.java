38731f78fd8874c764a35293331d5b771a08e27f
luye <luyeade@163.com>
2022-10-06 15:42:44 +0800

    101 tree

---
+++ tree/src/lc101SymmetricTree.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+public class lc101SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return helper(root.left, root.right);
+    }
+
+    public boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right != null || left != null && right == null) {
+            return false;
+        }
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
+}
