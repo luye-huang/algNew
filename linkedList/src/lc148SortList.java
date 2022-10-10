ed4805f6e5650378e96bc089bd56b98bba9ff9ed
luye <luyeade@163.com>
2022-10-10 10:54:38 +0800

    merge sort && linked list 148

--- linkedList/src/lc148SortList.java
+++ linkedList/src/lc148SortList.java
@@ -2,32 +2,48 @@ import org.junit.Test;
 
 public class lc148SortList {
     public ListNode sortList(ListNode head) {
-        ListNode dummy = new ListNode(Integer.MIN_VALUE);
-        ListNode node = head;
-        while (node != null) {
-            ListNode cur = dummy;
-            while (true) {
-                if (cur.next != null && cur.next.val > node.val) {
-                    ListNode addedNode = new ListNode(node.val);
-                    addedNode.next = cur.next;
-                    cur.next = addedNode;
-                    break;
-                } else if (cur.next == null) {
-                    cur.next = new ListNode(node.val);
-                    break;
-                } else {
-                    cur = cur.next;
-                }
-            }
-            node = node.next;
+        if (head == null || head.next == null)
+            return head;
+
+        // step 1. cut the list to two halves
+        ListNode prev = null, slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return dummy.next;
+
+        prev.next = null;
+
+        // step 2. sort each half
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+
+        // step 3. merge l1 and l2
+        return merge(l1, l2);
     }
 
     private ListNode merge(ListNode l1, ListNode l2) {
-        ListNode node1 = l1;
-        ListNode node2 = l2;
-        return l1;
+        ListNode head = new ListNode(0, l1);
+        ListNode node = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                node.next = l2;
+                l2 = l2.next;
+            } else {
+                node.next = l1;
+                l1 = l1.next;
+            }
+            node = node.next;
+        }
+        if (l1 != null) {
+            node.next = l1;
+        }
+        if (l2 != null) {
+            node.next = l2;
+        }
+        return head.next;
     }
 
     @Test
